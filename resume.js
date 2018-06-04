f092f52815c705f1de26e8932198ae00faac9c64
Haocen <haocen@v1020-wn-235-190.campus-dynamic.uwaterloo.ca>
2018-06-04 13:30:11 -0400

    notes added

--- resume.js
+++ resume.js
@@ -47,4 +47,3 @@ nav.addEventListener("mouseout", function(){
 })
 
 
-document.querySelector("head").innerHTML+="<meta name='viewport' content='width=device-width,initial-scale=1,shrink-to-fit=no'>"
